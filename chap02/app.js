8214414af775cf962974e9612c832245feb405fb
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-10 14:11:36 -0700

    Finished app.js --> server running on a port. Hello World

--- chap02/app.js
+++ chap02/app.js
@@ -1,1 +1,10 @@
-var http = require('http');
+var http = require('http')
+,ip = '127.0.0.1'
+,port = 8124;
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello, World!\n');
+}).listen(port, ip);
+
+console.log('Server runnning at http://'+ip+':'+port);
