e6ea8f44b06388e24a975fb6b3b1af5f38479779
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-11 12:31:56 -0700

    Chainable calls to server variable

--- chap04/simpleServer.js
+++ chap04/simpleServer.js
@@ -7,11 +7,12 @@ var http = require('http')
 
 // We can see although it was hidden in the implementation on chap02
 // That the server is based on EventEmitter
-server.on('request', function onRequest(request, response) {
+server
+.on('request', function onRequest(request, response) {
 	response
 	.writeHead(200, {'Content-Type': 'text/html'})
 	.end('<html><h1>Hello, World!</h1></html>');
-});
-server.listen(port, ip);
+})
+.listen(port, ip);
 
 console.log('Server runnning at http://'+ip+':'+port);
