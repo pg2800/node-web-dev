c01b84dd9992846efeaa3acf5a2f4330d50c624c
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-11 12:07:34 -0700

    Created a minimal publish and subscribe

--- chap04/pulser.js
+++ chap04/pulser.js
@@ -1,1 +1,29 @@
-ECHO is on.
+var events = require('events')
+,util = require('util');
+
+function Pulser() {
+	events.EventEmitter.call(this);
+}
+exports.Pulser = Pulser;
+
+// This does the whole work.
+util.inherits(Pulser, events.EventEmitter);
+
+Pulser.prototype.start = function() {
+	var self = this;
+	this.id = setInterval(function () {
+		util.log('>>>> pulse');
+		// Publish
+		self.emit('pulse');
+		util.log('<<<< pulse');
+	}, 1000);
+};
+
+exports.justStartThis = function (){
+	var pulser = new Pulser();
+	// Subscribe
+	pulser.on('pulse', function(){
+		util.log('pulse received');
+	});
+	pulser.start();
+};
