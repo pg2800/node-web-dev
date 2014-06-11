91f19dc9ef516d8557a87d16edd2d00f68e255a4
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-11 14:16:47 -0700

    Adding chainability to the response writeHead

--- chap04/moreComplexServer.js
+++ chap04/moreComplexServer.js
@@ -4,9 +4,75 @@ var http = require('http')
 ,os = require('os')
 
 ,server = http.createServer();
-server.on('request', function (request, response){
-	var request_url = url.parse(request.url, true);
-	if(request_url.pathname === '/'){
+server
+.on('request', function (request, response){
+	var writeHead = response.writeHead;
+	response.writeHead = function (){
+		old.apply(this, arguments);
+		return this;
+	};
+	// var end = response.end;
+	// response.end = function (){
+	// 	end.apply(this, arguments);
+	// 	return this;
+	// };
+
+
+	var request_url = url.parse(request.url, true/*???*/);
+	switch(request_url.pathname){
+		case '/':
+		// Right response
 		response.writeHead(200, {'Content-type': 'text/html'});
+		response.end(["<html><head><title>Hello, world!</title></head>",
+			"<body><h1>Hello, world!</h1>",
+			"<p><a href='/osinfo'>OS Info</a></p>",
+			"</body></html>"].join());
+		break;
+		case '/osinfo':
+		response
+		.writeHead(200, {'Content-type':'text/html'})
+		.end(["<html><head>",
+			"<title>Operating System Info</title></head>",
+			"<body><h1>Operating System Info</h1>",
+			"<table>",
+			"<tr><th>TMP Dir</th><td>{tmpdir}</td></tr>",
+			"<tr><th>Host Name</th><td>{hostname}</td></tr>",
+			"<tr><th>OS Type</th>",
+			"<td>{type} {osplat} {osarch} {osrelease}</td></tr>",
+			"<tr><th>Uptime</th>",
+			"    <td>{uptime} {loadavg}</td></tr>",
+			"<tr><th>Memory</th>",
+			"    <td>total: {totalmem} free:{freemem}</td></tr>",
+			"<tr><th>CPU's</th>",
+			"    <td><pre>{cpudata}</pre></td></tr>",
+			"<tr><th>Network</th>",
+			"    <td><pre>{netdata}</pre></td></tr>",
+			"</table>",
+			"</body></html>"]
+			.join('\n')
+			.replace("{tmpdir}", os.tmpDir())
+			.replace("{hostname}", os.hostname())
+			.replace("{type}", os.type())
+			.replace("{osplat}", os.platform())
+			.replace("{osarch}", os.arch())
+			.replace("{osrelease}", os.release())
+			.replace("{uptime}", os.uptime())
+			.replace("{loadavg}", util.inspect(os.loadavg()))
+			.replace("{totalmem}", os.totalmem())
+			.replace("{freemem}", os.freemem())
+			.replace("{cpudata}", util.inspect(os.cpus()))
+			.replace("{netdata}"),
+			util.inspect(os.networkInterfaces()));
+		//
+		break;
+		default:
+		response
+		// error
+		.writeHead(404, {'Content-type': 'text/plain'});
+		response.end("bad URL "+request.url);
+		break;
 	}
-});
+
+})
+.listen(8124);
+console.log('listening to http://localhost:8124');
