567ed7f0eb1686decf99ba0a01fdeabb19e3d5f1
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-11 12:15:15 -0700

    Minor semantic update

--- chap02/simpleServer.js
+++ chap02/simpleServer.js
@@ -2,9 +2,9 @@ var http = require('http')
 ,ip = '127.0.0.1'
 ,port = 8124;
 
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello, World!\n');
+http.createServer(function onRequest(request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hello, World!\n');
 }).listen(port, ip);
 
 console.log('Server runnning at http://'+ip+':'+port);
