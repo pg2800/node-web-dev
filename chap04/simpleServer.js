39918f18fb22d139f385a829648d7cf2ad8db2a4
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-11 12:21:40 -0700

    Finished the EventEmitter version

---
+++ chap04/simpleServer.js
@@ -0,0 +1,16 @@
+// node simpleServer.js
+// go to: http://127.0.0.1:8124 on a browser
+var http = require('http')
+,ip = '127.0.0.1'
+,port = 8124
+,server = http.createServer();
+
+// We can see although it was hidden in the implementation on chap02
+// That the server is based on EventEmitter
+server.on('request', function onRequest(request, response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.end('<html><h1>Hello, World!</h1></html>');
+});
+server.listen(port, ip);
+
+console.log('Server runnning at http://'+ip+':'+port);
