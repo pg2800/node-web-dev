7adedff32db71c8883e2c8f971a4d66bb60b7411
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-13 10:30:54 -0700

    Removed some comments. Also, removed the 404 that was executing every time even if the file was found.

--- chap04/fibonacci/app.js
+++ chap04/fibonacci/app.js
@@ -5,19 +5,12 @@ var express = require('express')
 ,cookieParser = require('cookie-parser')
 ,bodyParser = require('body-parser')
 
-,routes = require('./routes/index')
-,users = require('./routes/users')
-
 ,app = express();
 
 // view engine setup
 // This section sets up and configures the Express application and middleware
 // The app.set method provides settings for Express
 app
-
-// I added a port
-.set('port', process.env.PORT || 3000)
-
 .set('views', path.join(__dirname, 'views'))
 .set('view engine', 'ejs')
 
@@ -25,42 +18,32 @@ app
 .use(logger('dev'))
 .use(bodyParser.json())
 .use(bodyParser.urlencoded())
-.use(cookieParser())
-.use(express.static(path.join(__dirname, 'public')))
+.use(express.static(path.join(__dirname, 'public')));
 
-.use('/', routes)
-.use('/users', users)
+// catch 404 and forward to error handler
+// .use(function(req, res, next) {
+//     // var err = new Error('Not Found');
+//     // err.status = 404;
+//     // next();
+// });
 
-/// catch 404 and forward to error handler
-.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+// error handlers
+function errorHandler(dev){
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err
+            error: dev? err : {}
         });
     });
 }
 
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') errorHandler(true)
+
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
+else errorHandler();
 
 module.exports = app;
