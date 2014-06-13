ce2fb2b997e9ed509d75bfa83cfa090a6b557ca0
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-13 10:38:56 -0700

    Added debugging prints

--- chap04/fibonacci/routes/fibonacci.js
+++ chap04/fibonacci/routes/fibonacci.js
@@ -1,5 +1,7 @@
 var math = require('../math');
+// console.log("init");
 exports.index = function(request, response) {
+  console.log(arguments);
   if (request.query.fibonum) {
     response.render('fibonacci', {
       title: "Calculate Fibonacci numbers",
@@ -7,9 +9,11 @@ exports.index = function(request, response) {
       fiboval: math.fibonacci(request.query.fibonum)
     });
   } else {
+    console.log("render");
     response.render('fibonacci', {
       title: "Calculate Fibonacci numbers",
       fiboval: undefined
     });
   }
+  console.log("fin");
 };
