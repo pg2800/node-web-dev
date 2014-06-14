f0ebe3ee0b31575f8fe716b6ee46e2b8afbf8669
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-13 19:09:15 -0700

    Algorithm MergeSort

--- chap04/wget.js
+++ chap04/wget.js
@@ -6,14 +6,12 @@ var http = require('http')
 ,argUrl = process.argv[2]
 ,parsedUrl = url.parse(argUrl, true)
 ,options = {
-  host: null,
-  port: -1,
-  path: null,
+  host: parsedUrl.hostname,
+  port: parsedUrl.port,
+  path: parsedUrl.pathname,
   method: 'GET'
 };
-options.host = parsedUrl.hostname;
-options.port = parsedUrl.port;
-options.path = parsedUrl.pathname;
+
 if (parsedUrl.search) options.path += "?"+parsedUrl.search;
 var req = http.request(options, function(res) {
   util.log('STATUS: ' + res.statusCode);
