d56224974bfdd9428494a1f3bdcc44b068c475f5
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-13 20:13:31 -0700

    MergeSort finished

--- chap02/mergeSort.js
+++ chap02/mergeSort.js
@@ -1,35 +1,30 @@
 Object.defineProperty(Array.prototype, "merge", {
 	value: function (arr){
-		var self = this;
-		// arr = arr || [];
-		(function(s, l){
-			s.forEach(function (val, index){
-				if(l <= 0 || val <= arr[0]) return;
-				self.splice(index,0,arr.splice(0,1)[0]);
-				l--;
-			});
-			if(l>0) self = self.concat(arr);
-		})(self, arr.length);
+		var self = this
+		,length = self.length;
+
+		for (var index = 0, position = 0; index < length; index++, position++) {
+			while(arr[0] <= self[position]){
+				self.splice(position, 0, arr.splice(0,1)[0]);
+				position++;
+			}
+		}
+		if(arr.length > 0) self = self.concat(arr);
 		return self;
 	}
 });
 
 var mergeSort = exports.mergeSort = function (arr){
-	var length = arr.length;
-	return mergeSort_aux(arr, length, Math.floor(length/2));
+	return mergeSort_aux(arr, arr.length, Math.floor(arr.length/2));
 };
 
-var restLength;
 function mergeSort_aux(arr, totalLength, halfLength){
-	if(totalLength<=1) return arr;
-	restLength = totalLength - halfLength;
-	(function(arr){
-		arr.splice(0,halfLength)
-		console.log(arr);
-	})(arr);
-	return mergeSort_aux(arr.splice(0,halfLength), halfLength, Math.floor(halfLength/2))
-	.merge(mergeSort_aux(arr, restLength, Math.floor(restLength/2)));
+	if(totalLength <= 1) return arr;
+	var newHalfLength = Math.floor(halfLength/2);
+	return mergeSort_aux(arr.splice(0, halfLength), halfLength, newHalfLength)
+	.merge(mergeSort_aux(arr, totalLength - halfLength, halfLength - newHalfLength));
 }
 
-var testArr = [7,8,3,5,4,1,9,6,2,0];
-console.log(mergeSort(testArr));
+var testArr = [-1,7,8,3,5,4,1,9,6,-2,2,0,7,7,10,7,11];
+var result = mergeSort(testArr);
+console.log('R:['+result+']');
